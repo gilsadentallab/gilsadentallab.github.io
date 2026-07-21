5475aee91986dbec621f11932795676635be73b3
gilsadentallab <gilsadentallab@gmail.com>
2026-07-22 01:30:06 +0330

    Update script.js

--- script.js
+++ script.js
@@ -26,24 +26,3 @@ document.addEventListener("DOMContentLoaded", () => {
 
 });
 
-// Header background on scroll
-
-window.addEventListener("scroll", () => {
-
-    const header = document.querySelector("header");
-
-    if (window.scrollY > 50) {
-
-        header.style.background = "rgba(255,255,255,.98)";
-        header.style.borderBottom = "1px solid #e8e8e8";
-        header.style.boxShadow = "0 5px 20px rgba(0,0,0,.08)";
-
-    } else {
-
-        header.style.background = "rgba(255,255,255,.96)";
-        header.style.borderBottom = "1px solid #e8e8e8";
-        header.style.boxShadow = "none";
-
-    }
-
-});
