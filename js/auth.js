8037e2945ba993dd27c248d1a7065993b511864c
gilsadentallab <gilsadentallab@gmail.com>
2026-07-26 21:23:23 +0330

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -15,3 +15,53 @@ const registerContent = document.getElementById("register");
 const closeAuth = document.querySelector(".close-auth");
 
 const authButtons = document.querySelectorAll(".open-auth");
+/*==============================
+        OPEN MODAL
+==============================*/
+
+authButtons.forEach(button=>{
+
+    button.addEventListener("click",function(e){
+
+        e.preventDefault();
+
+        authModal.classList.add("show");
+
+        document.body.style.overflow="hidden";
+
+    });
+
+});
+/*==============================
+        CLOSE MODAL
+==============================*/
+
+function closeAuthModal(){
+
+    authModal.classList.remove("show");
+
+    document.body.style.overflow="auto";
+
+}
+
+closeAuth.addEventListener("click",closeAuthModal);
+
+authModal.addEventListener("click",function(e){
+
+    if(e.target===authModal){
+
+        closeAuthModal();
+
+    }
+
+});
+
+document.addEventListener("keydown",function(e){
+
+    if(e.key==="Escape"){
+
+        closeAuthModal();
+
+    }
+
+});
