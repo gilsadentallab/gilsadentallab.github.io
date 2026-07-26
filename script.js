991f2b165356c86a24c85ca51fef788ce2ac548c
gilsadentallab <gilsadentallab@gmail.com>
2026-07-27 00:47:47 +0330

    Update script.js

--- script.js
+++ script.js
@@ -716,9 +716,9 @@ window.openDashboard = function(){
         footer.style.display="none";
 
 
-    if(dashboard)
-        dashboard.style.display="flex";
-
+  if(dashboard){
+    dashboard.classList.add("active");
+}
 
     window.scrollTo(0,0);
 
