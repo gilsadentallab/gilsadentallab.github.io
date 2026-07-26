9c42ed3c4c61c662a00fcbe561aebeeaaddd6eb6
gilsadentallab <gilsadentallab@gmail.com>
2026-07-26 21:22:56 +0330

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,17 @@
+/*====================================
+        GILSA AUTH SYSTEM
+====================================*/
 
+const authModal = document.getElementById("authModal");
+
+const loginTab = document.querySelector('[data-tab="login"]');
+
+const registerTab = document.querySelector('[data-tab="register"]');
+
+const loginContent = document.getElementById("login");
+
+const registerContent = document.getElementById("register");
+
+const closeAuth = document.querySelector(".close-auth");
+
+const authButtons = document.querySelectorAll(".open-auth");
