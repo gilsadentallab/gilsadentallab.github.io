f62e09140d1f4d1726624e6c6b91aa6658d6749c
gilsadentallab <gilsadentallab@gmail.com>
2026-07-29 02:06:18 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,62 +1,19 @@
-/*==================================================
-            GILSA CORE APP V3
-==================================================*/
-
-const App = {
-
-    init(){
-
-        console.log(
-            "%cGILSA DENTAL LAB V3",
-            "color:#d4af37;font-size:18px;font-weight:bold;"
-        );
-
-        UI.init();
-
-        Auth.init();
-
-        Order.init();
-
-        Dashboard.init();
-
-        PriceList.init();
-
-    }
-
-};
-
-document.addEventListener(
-    "DOMContentLoaded",
-    ()=>App.init()
-);
-/*==================================================
-                APP START
-==================================================*/
-
 document.addEventListener(
 
 "DOMContentLoaded",
 
 ()=>{
 
-UI.init();
+Core.register(UI);
 
-Auth.init();
+Core.register(Auth);
 
-Order.init();
+Core.register(Order);
 
-Dashboard.init();
+Core.register(Dashboard);
 
-PriceList.init();
+Core.register(PriceList);
 
-console.log(
+Core.init();
 
-"%cGILSA V3 LOADED",
-
-"color:#d4af37;font-size:18px;font-weight:bold;"
-
-);
-
-}
-
-);
+});
