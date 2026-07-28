ee07cd741df8ad897e2b55bf8d17b316d1289ff8
gilsadentallab <gilsadentallab@gmail.com>
2026-07-29 03:04:47 +0330

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,20 +1,81 @@
+/*==================================================
+                GILSA APP V4
+==================================================*/
+
+
+import { Core } 
+from "./core.js";
+
+
+import { UI } 
+from "./ui.js";
+
+
+import { API } 
+from "./api.js";
+
+
+import { Auth } 
+from "./auth.js";
+
+
+import { Order } 
+from "./order.js";
+
+
+import { Dashboard } 
+from "./dashboard.js";
+
+
+import { PriceList } 
+from "./pricelist.js";
+
+
+
+
+
 document.addEventListener(
 
 "DOMContentLoaded",
 
 ()=>{
 
-Core.register(UI);
-Core.register(Services);
-  Core.register(API);
-Core.register(Auth);
 
-Core.register(Order);
 
-Core.register(Dashboard);
+    Core.register(UI);
 
-Core.register(PriceList);
 
-Core.init();
 
-});
+    Core.register(API);
+
+
+
+    Core.register(Auth);
+
+
+
+    Core.register(Order);
+
+
+
+    Core.register(Dashboard);
+
+
+
+    Core.register(PriceList);
+
+
+
+    Core.init();
+
+
+
+    console.log(
+        "Gilsa App V4 Loaded"
+    );
+
+
+
+}
+
+);
