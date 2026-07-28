15064b082c7895d0e463d5f3057e22ea2ed86421
gilsadentallab <gilsadentallab@gmail.com>
2026-07-29 01:58:19 +0330

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,1 +1,43 @@
+/*==================================================
+                UTILS MODULE V3
+==================================================*/
 
+const Utils = {
+
+    qs(selector){
+
+        return document.querySelector(selector);
+
+    },
+
+    qsa(selector){
+
+        return document.querySelectorAll(selector);
+
+    },
+
+    create(tag){
+
+        return document.createElement(tag);
+
+    },
+
+    formatDate(){
+
+        return new Date().toLocaleDateString("fa-IR");
+
+    },
+
+    generateId(){
+
+        return Date.now();
+
+    },
+
+    alert(message){
+
+        window.alert(message);
+
+    }
+
+};
