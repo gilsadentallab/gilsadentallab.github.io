47c90a13c47e1d45d2d345a489bbb8964245658a
gilsadentallab <gilsadentallab@gmail.com>
2026-07-29 02:08:47 +0330

    Update api.js

--- api.js
+++ api.js
@@ -1,1 +1,24 @@
+/*==================================================
+                API LAYER V4
+==================================================*/
 
+const API = {
+
+    provider: "local",
+
+    init(){
+
+        console.log(
+            "API Provider:",
+            this.provider
+        );
+
+    },
+
+    setProvider(provider){
+
+        this.provider = provider;
+
+    }
+
+};
