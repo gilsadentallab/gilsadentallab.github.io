6dfb0cfb768a907824a4502e85c6f8e643e10250
gilsadentallab <gilsadentallab@gmail.com>
2026-07-29 02:03:53 +0330

    Update app.js

--- app.js
+++ app.js
@@ -29,3 +29,34 @@ document.addEventListener(
     "DOMContentLoaded",
     ()=>App.init()
 );
+/*==================================================
+                APP START
+==================================================*/
+
+document.addEventListener(
+
+"DOMContentLoaded",
+
+()=>{
+
+UI.init();
+
+Auth.init();
+
+Order.init();
+
+Dashboard.init();
+
+PriceList.init();
+
+console.log(
+
+"%cGILSA V3 LOADED",
+
+"color:#d4af37;font-size:18px;font-weight:bold;"
+
+);
+
+}
+
+);
