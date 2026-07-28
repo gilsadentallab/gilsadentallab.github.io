fc39545a8dd7d861bdfbaf32be875c46ca01c3db
gilsadentallab <gilsadentallab@gmail.com>
2026-07-29 02:54:54 +0330

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,53 +1,109 @@
 /*==================================================
-                STORAGE MODULE V3
+                GILSA STORAGE V4
 ==================================================*/
+
 
 const Storage = {
 
+
     get(key){
+
 
         try{
 
-            return JSON.parse(localStorage.getItem(key));
 
-        }catch{
+            const data =
+            localStorage.getItem(key);
+
+
+            if(!data) return null;
+
+
+            return JSON.parse(data);
+
+
+        }
+        catch{
+
 
             return localStorage.getItem(key);
 
+
         }
 
+
     },
+
+
+
+
 
     set(key,value){
 
-        if(typeof value === "object"){
+
+
+        if(
+            typeof value === "object"
+        ){
+
 
             localStorage.setItem(
+
                 key,
+
                 JSON.stringify(value)
+
             );
 
-        }else{
-
-            localStorage.setItem(
-                key,
-                value
-            );
 
         }
 
+        else{
+
+
+            localStorage.setItem(
+
+                key,
+
+                value
+
+            );
+
+
+        }
+
+
     },
+
+
+
+
 
     remove(key){
 
+
         localStorage.removeItem(key);
+
 
     },
 
+
+
+
+
     clear(){
+
 
         localStorage.clear();
 
+
     }
 
+
+
 };
+
+
+
+
+export { Storage };
