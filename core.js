b3725a33b74ed7e286130cf30a005045d319e19b
gilsadentallab <gilsadentallab@gmail.com>
2026-07-29 02:05:25 +0330

    Update core.js

--- core.js
+++ core.js
@@ -1,1 +1,43 @@
+/*==================================================
+                GILSA CORE V4
+==================================================*/
 
+const Core = {
+
+version:"4.0.0",
+
+started:false,
+
+modules:[],
+
+register(module){
+
+this.modules.push(module);
+
+},
+
+init(){
+
+if(this.started) return;
+
+this.started=true;
+
+this.modules.forEach(module=>{
+
+if(typeof module.init==="function"){
+
+module.init();
+
+}
+
+});
+
+console.log(
+
+`Gilsa V${this.version} Started`
+
+);
+
+}
+
+};
