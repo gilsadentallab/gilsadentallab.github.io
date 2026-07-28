b117c3dacf1a7bbbbc80a222f7f08c72e2fbabaf
gilsadentallab <gilsadentallab@gmail.com>
2026-07-29 02:54:26 +0330

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,8 +1,10 @@
 /*==================================================
-                UTILS MODULE V3
+                GILSA UTILS V4
 ==================================================*/
 
+
 const Utils = {
+
 
     qs(selector){
 
@@ -10,11 +12,15 @@ const Utils = {
 
     },
 
+
+
     qsa(selector){
 
         return document.querySelectorAll(selector);
 
     },
+
+
 
     create(tag){
 
@@ -22,22 +28,99 @@ const Utils = {
 
     },
 
+
+
     formatDate(){
 
-        return new Date().toLocaleDateString("fa-IR");
+
+        return new Date()
+        .toLocaleDateString("fa-IR");
+
 
     },
+
+
 
     generateId(){
 
+
         return Date.now();
+
 
     },
 
+
+
     alert(message){
+
 
         window.alert(message);
 
+
+    },
+
+
+
+    storage(key,value=null){
+
+
+        if(value === null){
+
+
+            try{
+
+                return JSON.parse(
+                    localStorage.getItem(key)
+                );
+
+
+            }
+            catch{
+
+                return localStorage.getItem(key);
+
+            }
+
+
+        }
+
+
+
+        if(typeof value === "object"){
+
+
+            localStorage.setItem(
+
+                key,
+
+                JSON.stringify(value)
+
+            );
+
+
+        }
+
+        else{
+
+
+            localStorage.setItem(
+
+                key,
+
+                value
+
+            );
+
+
+        }
+
+
     }
 
+
+
 };
+
+
+
+export { Utils };
