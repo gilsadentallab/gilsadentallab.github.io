788f1f75bccb793c6f342f6b72d503466a0020fe
gilsadentallab <gilsadentallab@gmail.com>
2026-07-29 02:09:11 +0330

    Update api.js

--- api.js
+++ api.js
@@ -22,3 +22,52 @@ const API = {
     }
 
 };
+API.auth = {
+
+    async login(username,password){
+
+        return Services.auth.login(
+            username,
+            password
+        );
+
+    },
+
+    async register(user){
+
+        return Services.auth.register(
+            user
+        );
+
+    },
+
+    async logout(){
+
+        return Services.auth.logout();
+
+    }
+
+};
+API.orders = {
+
+    async getMine(){
+
+        return Services.orders.mine();
+
+    },
+
+    async getAll(){
+
+        return Services.orders.all();
+
+    },
+
+    async save(list){
+
+        return Services.orders.save(
+            list
+        );
+
+    }
+
+};
